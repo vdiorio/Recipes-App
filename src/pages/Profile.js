d004fabcc7ecd9c468f532e5ec372d364294e3ff
Rodrigo Moreira <rodrigossmoreira@hotmail.com>
2022-01-05 14:16:47 -0300

    requisitos 82 a 87

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Header from '../components/Header';
 
 export default function Profile() {
   return (
     <div>
-      Profile
+      <Header />
+      <label htmlFor="email-profile">
+        Puxar email do login/localStorage
+        <input
+          data-testid="profile-email"
+          id="email-profile"
+          name="email-profile"
+        />
+      </label>
+      <Link to="/receitas-feitas">
+        <button
+          data-testid="profile-done-btn"
+          type="button"
+        >
+          Receitas Feitas
+        </button>
+      </Link>
+      <Link to="/receitas-favoritas">
+        <button
+          data-testid="profile-favorite-btn"
+          type="button"
+        >
+          Receitas Favoritas
+        </button>
+      </Link>
+      <button
+        data-testid="profile-logout-btn"
+        type="button"
+      >
+        Sair
+      </button>
     </div>
   );
 }
