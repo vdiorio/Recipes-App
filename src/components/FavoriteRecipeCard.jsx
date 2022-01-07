3e05434eac4b58f29c613d08388dd06d1aaf1210
Vitor Diorio <vitor.diorio@yahoo.com.br>
2022-01-07 15:46:09 -0300

    Página de receitas favoritas concluída

---
+++ src/components/FavoriteRecipeCard.jsx
@@ -0,0 +1,110 @@
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import './DoneRecipeCard.css';
+import { Link } from 'react-router-dom';
+import shareIcon from '../images/shareIcon.svg';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
+
+const copy = require('clipboard-copy');
+
+export default function FavoriteRecipeCard({ recipe, index, removeFavorite }) {
+  const [showToast, setShowToast] = useState(
+    <span className="copied-link">Link copiado!</span>,
+  );
+
+  // Função criada separadamente pq o toast gerado no context ativaria para todos os cards simultaneamente
+  function showIndividualToast(link) {
+    const THREE_SECONDS = 3000;
+    copy(link);
+    setShowToast(
+      <span className="copied-link copied-link--active">Link copiado!</span>,
+    );
+    return setTimeout(() => {
+      setShowToast(
+        <span className="copied-link">Link copiado!</span>,
+      );
+    }, THREE_SECONDS);
+  }
+
+  const {
+    type,
+    id,
+    category,
+    alcoholicOrNot,
+    area,
+    image,
+    name,
+  } = recipe;
+  const link = window.location.href.split('/');
+  link[3] = `${type}s`;
+  link[4] = id; // Tratamento do link para o botão de compartilhamento
+  return (
+    <div // Container do card
+      className="done-card"
+      data-testid={ `${index}-recipe-card` }
+    >
+      <Link to={ `/${type}s/${id}` }>
+        <img
+          className="card-image"
+          src={ image }
+          alt={ `${area} meal` }
+          data-testid={ `${index}-horizontal-image` }
+          width={ 130 }
+        />
+      </Link>
+      <div>
+        <div>
+          <span className="recipe-type" data-testid={ `${index}-horizontal-top-text` }>
+            { `${alcoholicOrNot.length > 0 ? alcoholicOrNot : area} - ${category}` }
+          </span>
+          <Link to={ `/${type}s/${id}` }>
+            <h6 data-testid={ `${index}-horizontal-name` }>
+              {name}
+            </h6>
+          </Link>
+        </div>
+        <button
+          type="button"
+          className="media-btn"
+          onClick={ () => showIndividualToast(link.join('/')) }
+          style={ { marginTop: 20 } }
+        >
+          <img
+            data-testid={ `${index}-horizontal-share-btn` }
+            src={ shareIcon }
+            alt="Share Icon"
+            className="media-btn-img"
+          />
+        </button>
+        {showToast}
+        <button
+          type="button"
+          className="media-btn"
+          style={ { marginTop: 20 } }
+          onClick={ () => removeFavorite(index) }
+        >
+          <img
+            data-testid={ `${index}-horizontal-favorite-btn` }
+            src={ blackHeartIcon }
+            alt="Share Icon"
+            className="media-btn-img"
+          />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+FavoriteRecipeCard.propTypes = {
+  index: PropTypes.number.isRequired,
+  recipe: PropTypes.shape({
+    alcoholicOrNot: PropTypes.string.isRequired,
+    area: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+  }).isRequired,
+  removeFavorite: PropTypes.func.isRequired,
+};
--- src/pages/FavoritesRecipes.js
+++ src/pages/FavoritesRecipes.js
@@ -1,11 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import GenericHeader from '../components/GenericHeader';
+import FavoriteRecipeCard from '../components/FavoriteRecipeCard';
 
-export default function FavoritesRecipes() {
+export default function FavoriteRecipes() {
+  const [favoriteRecipes, setRecipes] = useState([]);
+  const [filter, setFilter] = useState('all');
+
+  useEffect(() => { // Ao montar o componente, recupera as receitas no localstorage e as salva no estado
+    const recipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
+    setRecipes(recipes);
+  }, []);
+
   const value = 'Receitas Favoritas';
+  const categorias = ['all', 'food', 'drink']; // Array para economizar código repetitivo
+
+  async function removeFavorite(index) {
+    const filteredArray = [...favoriteRecipes].filter((_rec, i) => i !== index);
+    setRecipes(filteredArray);
+    localStorage.setItem('favoriteRecipes', JSON.stringify(filteredArray));
+  }
+
   return (
     <div>
       <GenericHeader value={ value } />
+      <div // Container dos botões de filtro tentei usar os mesmos da página inicial, mas os testes existem id's e tratamentos difernetes
+        className="btn-group"
+        role="group"
+        aria-label="Basic radio toggle button group"
+      >
+        { categorias.map((cat, i) => {
+          let filterName = cat === 'food' ? 'comida' : 'bebida'; // o filtro está sendo salvo no localStorage com os nomes em portugues
+          if (cat === 'all') filterName = cat; // para poder filtrar corretamente criei essa lógica para simplificar o estado do filtro
+          return (
+            <div key={ i }>
+              <input
+                type="checkbox"
+                className="btn-check"
+                name="btnradio"
+                id={ cat }
+                autoComplete="off"
+                onChange={ () => setFilter(filterName) }
+                checked={ filter === filterName }
+              />
+              <label
+                className="btn btn-outline-primary"
+                htmlFor={ cat }
+                data-testid={ `filter-by-${cat}-btn` }
+              >
+                { cat }
+                <input className="btn-check" type="radio" />
+              </label>
+            </div>
+          );
+        }) }
+      </div>
+      { Array.isArray(favoriteRecipes) && favoriteRecipes // Sem checar se recipes done é um array a pagina não carrega em alguns testes
+        .filter((rec) => filter === 'all' || rec.type === filter)
+        .map((rec, i) => (
+          <FavoriteRecipeCard
+            key={ i }
+            recipe={ rec }
+            index={ i }
+            removeFavorite={ (id) => removeFavorite(id) }
+          />
+        )) }
     </div>
   );
 }
