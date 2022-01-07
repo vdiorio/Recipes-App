6e63e5eff019d74f72e3a0b1715e7fe5c7e9953a
Valdeci <moura97.valdeci@gmail.com>
2022-01-07 18:35:16 -0300

    Cria pasta e alguns de mocks para os testes.

---
+++ src/tests/mocks/foodCategories.js
@@ -0,0 +1,51 @@
+const foodCategories = {
+  link: {
+    url: 'https://www.themealdb.com/api/json/v1/1/list.php?c=list',
+  },
+  meals: [
+    {
+      strCategory: 'Beef',
+    },
+    {
+      strCategory: 'Breakfast',
+    },
+    {
+      strCategory: 'Chicken',
+    },
+    {
+      strCategory: 'Dessert',
+    },
+    {
+      strCategory: 'Goat',
+    },
+    {
+      strCategory: 'Lamb',
+    },
+    {
+      strCategory: 'Miscellaneous',
+    },
+    {
+      strCategory: 'Pasta',
+    },
+    {
+      strCategory: 'Pork',
+    },
+    {
+      strCategory: 'Seafood',
+    },
+    {
+      strCategory: 'Side',
+    },
+    {
+      strCategory: 'Starter',
+    },
+    {
+      strCategory: 'Vegan',
+    },
+    {
+      strCategory: 'Vegetarian',
+    },
+  ],
+};
+
+module.exports = foodCategories;
